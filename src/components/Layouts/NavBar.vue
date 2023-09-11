5cc73e2387ed285a26b5ed36e068799d111c2fe3
wahyuPM <wahyupratama191@gmail.com>
2023-09-11 13:18:20 +0800

    add : notes card design

--- src/components/Layouts/NavBar.vue
+++ src/components/Layouts/NavBar.vue
@@ -1,27 +1,48 @@
 <template>
     <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <div class="navbar-item is-size-4 is-family-monospace">
-                Noteballs
+        <div class="container is-max-dekstop px-2">
+            <div class="navbar-brand">
+                <div class="navbar-item is-size-4 is-family-monospace">
+                    Noteballs
+                </div>
+
+                <a @click.prevent="showMobileNav = !showMobileNav" role="button" class="navbar-burger"
+                    :class="{ 'is-active': showMobileNav }" aria-label="menu" aria-expanded="false"
+                    data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
             </div>
 
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
+            <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
+                <div class="navbar-end">
+                    <RouterLink to="/" class="navbar-item" active-class="is-active">
+                        Notes
+                    </RouterLink>
 
-        <div id="navbarBasicExample" class="navbar-menu">
-            <div class="navbar-end">
-                <RouterLink to="/" class="navbar-item" active-class="is-active">
-                    Notes
-                </RouterLink>
-
-                <RouterLink to="/stats" class="navbar-item" active-class="is-active">
-                    Stats
-                </RouterLink>
+                    <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+                        Stats
+                    </RouterLink>
+                </div>
             </div>
         </div>
     </nav>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showMobileNav = ref(false)
+</script>
+
+<style>
+@media (max-width: 1023px) {
+    .navbar-menu {
+        position: absolute;
+        width: 100%;
+        left: 0;
+
+    }
+}
+</style>
--- src/views/ViewNote.vue
+++ src/views/ViewNote.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="notes">
-        <h1>Notes</h1>
+        <div class="card mb-4" v-for="item in 3">
+            <div class="card-content">
+                <div class="content">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa aut odit aliquid vero, mollitia
+                    explicabo, ratione voluptas rem porro distinctio vel veniam blanditiis esse aperiam doloribus possimus
+                    voluptatum. Laborum, aut.
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
     </div>
 </template>
