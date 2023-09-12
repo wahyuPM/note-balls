2b1e48552f41e89c891266620691a8c3726d957b
wahyuPM <wahyupratama191@gmail.com>
2023-09-12 22:44:11 +0800

    add : newNote function

--- src/views/ViewNote.vue
+++ src/views/ViewNote.vue
@@ -3,22 +3,21 @@
         <div class="card has-background-success-dark p-4 mb-5">
             <div class="field">
                 <div class="control">
-                    <textarea class="textarea" placeholder="Add new note"></textarea>
+                    <textarea v-model="newNote" class="textarea" placeholder="Add new note" ref="newNoteRef"></textarea>
                 </div>
             </div>
 
             <div class="field is-grouped is-grouped-right">
                 <div class="control">
-                    <button class="button is-link has-background-success">Add New Note</button>
+                    <button @click="addNote" :disabled="!newNote" class="button is-link has-background-success">Add New
+                        Note</button>
                 </div>
             </div>
         </div>
-        <div class="card mb-4" v-for="item in 3">
+        <div class="card mb-4" v-for="note in notes" :key="note.id">
             <div class="card-content">
                 <div class="content">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa aut odit aliquid vero, mollitia
-                    explicabo, ratione voluptas rem porro distinctio vel veniam blanditiis esse aperiam doloribus possimus
-                    voluptatum. Laborum, aut.
+                    {{ note.content }}
                 </div>
             </div>
             <footer class="card-footer">
@@ -28,3 +27,37 @@
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const newNote = ref('')
+const newNoteRef = ref(null)
+
+const notes = ref([
+    {
+        id: 'id1',
+        content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa aut odit aliquid vero, mollitia explicabo, ratione voluptas rem porro distinctio vel veniam blanditiis esse aperiam doloribus possimus voluptatum.Laborum, aut.'
+    },
+    {
+        id: 'id2',
+        content: 'This is shorter note! woo!'
+    }
+])
+
+const addNote = () => {
+    let currentDate = new Date().getTime(),
+        id = currentDate.toString()
+
+    let note = {
+        id,
+        content: newNote.value
+    }
+
+    notes.value.unshift(note)
+
+    newNote.value = ''
+    newNoteRef.value.focus()
+}
+
+</script>
